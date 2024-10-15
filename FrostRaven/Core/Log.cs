8a433d27a621c64faecf83b2cd7ba83b21dd5345
insotheo <maxim.insotheo@gmail.com>
2024-10-15 18:55:58 +0400

    Project setup

---
+++ FrostRaven/Core/Log.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace FrostRaven.Core
+{
+    public static class Log
+    {
+#if DEBUG
+        public static bool IsEnabled = true;
+#else
+        public static bool IsEnabled = false;
+#endif
+
+        public static string DefaultSender = "GameCore";
+
+        private static void write(string message, string sender)
+        {
+            if (!IsEnabled || String.IsNullOrEmpty(message))
+            {
+                return;
+            }
+            if (String.IsNullOrEmpty(sender))
+            {
+                sender = DefaultSender;
+            }
+            Console.WriteLine($"[{DateTime.Now.ToString("T")}] {sender}: \"{message}\"");
+        }
+
+        public static void Trace<T>(T message, string sender = "")
+        {
+            if(message == null)
+            {
+                return;
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            write(message.ToString(), sender);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Info<T>(T message, string sender = "")
+        {
+            if (message == null)
+            {
+                return;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            write(message.ToString(), sender);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Warn<T>(T message, string sender = "")
+        {
+            if (message == null)
+            {
+                return;
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            write(message.ToString(), sender);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Error<T>(T message, string sender = "")
+        {
+            if (message == null)
+            {
+                return;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            if(message is Exception exeption)
+            {
+                write(exeption.Message, sender);
+            }
+            else
+            {
+                write(message.ToString(), sender);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
---
+++ SandBox/Program.cs
@@ -0,0 +1,13 @@
+﻿using FrostRaven.Core;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main()
+        {
+            Log.DefaultSender = "SandBox";
+            Log.Trace("Hello, World!");
+        }
+    }
+}
