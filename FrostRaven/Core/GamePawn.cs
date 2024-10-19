d075d6667fa0d8b502a8063015b02320841030d3
insotheo <maxim.insotheo@gmail.com>
2024-10-19 16:49:54 +0400

    GamePawn

---
+++ FrostRaven/Core/GamePawn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FrostRaven.Core
+{
+    public class GamePawn : IDisposable
+    {
+
+        internal void OnPawnBegin() => OnBegin();
+        internal void OnPawnUpdate(float dt) => OnUpdate(dt);
+
+        protected virtual void OnBegin() { }
+        protected virtual void OnUpdate(float dt) { }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
